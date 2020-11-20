bd810d681e58d44a8f8c7074a169ab559779812f
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-20 15:53:18 +0100

    added some attributes to DocumentSnapshot

--- cloud_firestore_server/lib/src/document_snapshot.dart
+++ cloud_firestore_server/lib/src/document_snapshot.dart
@@ -1,11 +1,22 @@
 import 'document_reference.dart';
 import 'snapshot_metadata.dart';
+import 'timestamp.dart';
 
 class DocumentSnapshot {
   DocumentSnapshot(this.id, this._data, {this.exists});
 
   /// This document's given ID for this snapshot.
   final String id;
+
+  /// Returns `true` if the [DocumentSnapshot] exists.
+  ///
+  /// ```dart
+  /// final documentSnapshot = await firestore.doc('col/doc').get();
+  /// if(documentSnapshot.exists) {
+  ///   print('Data: ${documentSnapshot.data()}');
+  /// }
+  /// ```
+  final bool exists;
 
   /// Returns the [DocumentReference] of this snapshot.
   @Deprecated('Unimplemented')
@@ -20,8 +31,12 @@ class DocumentSnapshot {
     throw UnimplementedError();
   }
 
-  /// Returns `true` if the [DocumentSnapshot] exists.
-  final bool exists;
+  ///  The time the document was created. Null for documents that don't
+  ///  exist.
+  @Deprecated('Unimplemented')
+  Timestamp get createdTime {
+    throw UnimplementedError();
+  }
 
   final Map<String, dynamic> _data;
 
