1274e367b85b52bb49a3b3d50f2544c7a2f99532
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-24 20:57:11 +0100

    Added CollectionReference methods and docs.

--- cloud_firestore_server/lib/src/collection_reference.dart
+++ cloud_firestore_server/lib/src/collection_reference.dart
@@ -1,8 +1,14 @@
+import 'package:cloud_firestore_server/cloud_firestore_server.dart';
+import 'package:quiver/core.dart';
+
 import 'document_reference.dart';
 import 'internal/instance_resources.dart';
 import 'internal/pointer.dart';
 import 'query.dart';
 
+/// A CollectionReference object can be used for adding documents, getting
+/// document references, and querying for documents (using the methods
+/// inherited from [Query]).
 class CollectionReference extends Query {
   CollectionReference(
     InstanceResources instanceResources, {
@@ -12,8 +18,121 @@ class CollectionReference extends Query {
           path: path,
         );
 
-  DocumentReference doc(String documentPath) {
+  /// The [Firestore] Instance for this Firestore Database.
+  @Deprecated('Unimplemented')
+  Firestore get firestore {
+    throw UnimplementedError();
+  }
+
+  /// The last path element of the referenced document.
+  ///
+  /// ```dart
+  /// assert(firestore.doc('col/my-doc').id == 'my-doc');
+  /// ```
+  String get id {
+    return Pointer(path).id;
+  }
+
+  /// A reference to the containing Document if this is a subcollection, else
+  /// null.
+  ///
+  /// ```dart
+  /// final collectionReference = firestore.doc('col/doc/my-collection');
+  /// assert(collectionReference.parent == firestore.doc('col/doc'));
+  ///
+  /// final collectionReference = firestore.doc('col');
+  /// assert(collectionReference.parent == null);
+  /// ```
+  @Deprecated('Unimplemented')
+  CollectionReference? get parent {
+    throw UnimplementedError();
+  }
+
+  /// A string representing the path of the referenced collection (relative
+  /// to the root of the database).
+  @override
+  String get path {
+    return super.path;
+  }
+
+  /// Gets a [DocumentReference] instance that refers to the document at the
+  /// specified path.
+  /// If no path is specified, an automatically-generated unique ID will be used
+  /// for the returned DocumentReference. (CURRENTLY NOT IMPLEMENTED)
+  ///
+  /// ```dart
+  /// final collectionRef = firestore.collection('col');
+  /// final documentRefWithName = collectionRef.doc('doc');
+  /// final documentRefWithAutoId = collectionRef.doc();
+  /// print('Reference with name: ${documentRefWithName.path}');
+  /// print('Reference with auto-id: ${documentRefWithAutoId.path}');
+  /// ```
+  DocumentReference doc(String? documentPath) {
+    if (documentPath == null) {
+      throw UnimplementedError('Automatic ID generation not implemented');
+    }
     final _documentPath = Pointer(path).documentPath(documentPath);
     return DocumentReference(instanceResources, path: _documentPath);
+  }
+
+  /// Retrieves the list of documents in this collection.
+  ///
+  /// The document references returned may include references to "missing
+  /// documents", i.e. document locations that have no document present but
+  /// which contain subcollections with documents. Attempting to read such a
+  /// document reference (e.g. via `.get()` or `.onSnapshot()`) will return a
+  /// [DocumentSnapshot] whose `.exists` property is false.
+  ///
+  /// ```dart
+  /// final collectionRef = firestore.collection('col');
+  ///
+  /// final documentRefs = await collectionRef.listDocuments();
+  /// final documentSnapshots = await firestore.getAll(documentRefs);
+  ///
+  /// for (final documentSnapshot in documentSnapshots) {
+  ///    if (documentSnapshot.exists) {
+  ///      print('Found document with data: ${documentSnapshot.id}');
+  ///    } else {
+  ///      print('Found missing document: ${documentSnapshot.id}');
+  ///    }
+  /// }
+  ///
+  /// ```
+  @Deprecated('Unimplemented')
+  Future<List<DocumentReference>> listDocuments() {
+    throw UnimplementedError();
+  }
+
+  /// Add a new document to this collection with the specified data, assigning
+  /// it a document ID automatically.
+  ///
+  /// Returns a [DocumentReference] pointing to the newly created document.
+  ///
+  /// ```dart
+  /// final collectionRef = firestore.collection('col');
+  /// final documentReference = await collectionRef.add({foo: 'bar'});
+  /// print('Added document with name: ${documentReference.id}');
+  /// ```
+  @Deprecated('Unimplemented')
+  Future<DocumentReference> add(Map<String, dynamic> data) {
+    throw UnimplementedError();
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        other is CollectionReference &&
+            other.path == path &&
+            other.instanceResources.databasePath ==
+                instanceResources.databasePath;
+  }
+
+  @override
+  int get hashCode =>
+      hash2(path.hashCode, instanceResources.databasePath.hashCode);
+
+  @override
+  String toString() {
+    return 'CollectionReference(path: $path)';
   }
 }
