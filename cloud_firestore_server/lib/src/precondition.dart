647e0ad712940a34e5e0c28c6f1e7c96ca499654
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-24 22:55:00 +0100

    removed Precondition named constructors

--- cloud_firestore_server/lib/src/precondition.dart
+++ cloud_firestore_server/lib/src/precondition.dart
@@ -2,14 +2,8 @@ import 'timestamp.dart';
 
 @Deprecated('Unimplemented')
 class Precondition {
-  @Deprecated('Unimplemented')
-  // ignore: avoid_unused_constructor_parameters, avoid_positional_boolean_parameters
-  factory Precondition.exists(bool exists) {
-    throw UnimplementedError();
-  }
-  @Deprecated('Unimplemented')
-  // ignore: avoid_unused_constructor_parameters, avoid_positional_boolean_parameters
-  factory Precondition.updateTime(Timestamp timestamp) {
-    throw UnimplementedError();
-  }
+  final bool? exists;
+  final Timestamp? lastUpdateTime;
+
+  Precondition({this.exists, this.lastUpdateTime});
 }
