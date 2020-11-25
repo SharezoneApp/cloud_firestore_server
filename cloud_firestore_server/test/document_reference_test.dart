f1bc7b8a5760693abd0dec7f5ab731d45d0c353a
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2020-11-25 22:05:43 +0100

    fixed lints and removed/skipped some unimplemented tests

--- cloud_firestore_server/test/document_reference_test.dart
+++ cloud_firestore_server/test/document_reference_test.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore_server/cloud_firestore_server.dart';
 import 'package:test/test.dart';
 
+// Easier to read in tests:
+// ignore_for_file: prefer_function_declarations_over_variables
+
 Future<void> main() async {
   final firestore = await Firestore.internal(url: 'http://localhost:8080/');
   group('DocumentReference', () {
@@ -17,39 +20,42 @@ Future<void> main() async {
       expect(doc.exists, false);
     });
     test(
-        '.delte with .lastUpdateTime fails if a document was not last updated at given lastUpdateTime',
-        () async {
-      // Arrange
-      final docRef = firestore.doc('col/doc123');
-      await docRef.set({'foo': 'bar'});
+      '.delte with .lastUpdateTime fails if a document was not last updated at given lastUpdateTime',
+      () async {
+        // Arrange
+        final docRef = firestore.doc('col/doc123');
+        await docRef.set({'foo': 'bar'});
 
-      // Act
-      final exec = () => docRef.delete(
-          precondition: Precondition(lastUpdateTime: Timestamp.now()));
+        // Act
+        final exec = () => docRef.delete(
+            precondition: Precondition(lastUpdateTime: Timestamp.now()));
 
-      // Assert
-      expect(exec, throws);
-    });
+        // Assert
+        expect(exec, throwsA(isA<Exception>()));
+      },
+      skip: '.delete throws atm because I cant get lastUpdateTime to work.',
+    );
 
     test(
-        '.delte with .lastUpdateTime succeeds if document was last updated at given lastUpdateTime',
-        () async {
-      // Arrange
-      final docRef = firestore.doc('col/doc234');
-      await docRef.set({'foo': 'bar'});
-      final doc = await docRef.get();
-      final updateTime = doc.updateTime;
+      '.delte with .lastUpdateTime succeeds if document was last updated at given lastUpdateTime',
+      () async {
+        // Arrange
+        final docRef = firestore.doc('col/doc234');
+        await docRef.set({'foo': 'bar'});
+        final doc = await docRef.get();
+        final updateTime = doc.updateTime;
 
-      // Act
-      await docRef.delete(
-          precondition: Precondition(lastUpdateTime: updateTime));
+        // Act
+        await docRef.delete(
+            precondition: Precondition(lastUpdateTime: updateTime));
 
-      // Assert
-      final deletedDoc = await docRef.get();
-      expect(doc.exists, false);
-    },
-        skip:
-            'Does not work - fails with "the stored version (1606336943323081) does not match the required base version (0)"');
+        // Assert
+        final deletedDoc = await docRef.get();
+        expect(deletedDoc.exists, false);
+      },
+      skip:
+          'Does not work - fails with "the stored version (1606336943323081) does not match the required base version (0)"',
+    );
 
     test('.path example test', () async {
       final firestore = await Firestore.internal();
