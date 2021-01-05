1f16549606b698af1c255dae9f91003dfc0a910e
Jonas Sander <29028262+Jonas-Sander@users.noreply.github.com>
2021-01-05 22:52:49 +0100

    Deleted/Commented test workflow.

--- scripts/load_env_into_bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Load environment variables from .env file
-export $(grep -v '#.*' .env | xargs)
--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# On CI-Pipeline env-vars are pre-exported via Github secrets not via .env file.
-if [ -f ".env" ]; then
-    # Must be ". script" so that vars are exported to this shell
-    . ./load_env_into_bash.sh
-fi
+echo "This is not yet implemented. See https://github.com/SharezoneApp/cloud_firestore_server/issues/3"
+exit 1
 
 cd cloud_firestore_server
 dart pub get
